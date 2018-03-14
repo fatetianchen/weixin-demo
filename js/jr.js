d2106d6df8aca8c6e435c43488f0f402c4646bc4
fatetianchen <398272208@qq.com>
2018-03-14 18:27:50 +0800

    first commit

---
+++ js/jr.js
@@ -0,0 +1,85 @@
+$("#name").blur(function() {
+	         	var user = $("#name").val();
+	         	var name = /^[a-zA-Z\u4e00-\u9fa5]+$/;
+	         	if(user.length == "") {
+	         		$("#name-text").text("名字不能为空").css('color','#fe4a4a');
+	         		return false;
+	         	}else if(!name.test(user)){
+	         		$("#name-text").text("名字不正确").css('color','#fe4a4a');
+					return false;
+	         	}else{
+	         		$("#name-text").html("输入正确").css('color','green');
+					return false;
+	         	}
+	         })
+			$("#phone").blur(function isPhoneNo() {
+	         	var user = $("#phone").val();
+	         	var pattern = /^1[34578]\d{9}$/; 
+	         	if(user.length == "") {
+	         		$("#phone-text").text("号码不能为空").css('color','#fe4a4a');
+	         		return false;
+	         	}else if(!pattern.test(user)){
+					$("#phone-text").html("请输入11位号码").css('color','#fe4a4a');
+					return false;
+	         	}else {
+	         		$("#phone-text").html("输入正确").css('color','green');
+					return false;
+	         	}
+	         })	
+	$("#rens").blur(function() {
+	         	var user = $("#rens").val();
+	         	if(user.length == "") {
+	         		$("#rens-text").text("人数不能为空").css('color','#fe4a4a');
+	         		return false;
+	         	}else{
+	         		$("#rens-text").html("输入正确").css('color','green');
+					return false;
+	         	}
+	         })
+	function isPhoneNo(phone) {   
+		var pattern = /^1[34578]\d{9}$/;   
+		return pattern.test(phone);   
+	}  
+	function isChinaName(name) {  
+		var pattern = /^[a-zA-Z\u4e00-\u9fa5]+$/;  
+		return pattern.test(name);  
+	} 
+
+		function formValidate() {
+		 var str = '';
+		 //判断人数
+		if($.trim($('#rens').val()).length == 0) {
+			str += '人数没有输入\n';
+			$('#rens').focus();
+		}
+		 // 判断名称
+		if($.trim($('#name').val()).length == 0) {
+			str += '姓名没有输入\n';
+			$('#name').focus();
+		}else if(isChinaName($.trim($('#name').val())) == false){
+			str += '姓名不正确\n';
+			$('#name').focus();
+		}
+		 
+		 // 判断手机号码
+		if ($.trim($('#phone').val()).length == 0) { 
+			str += '手机号没有输入\n';
+			$('#phone').focus();
+		} else if(isPhoneNo($.trim($('#phone').val())) == false){
+			str += '手机号码不正确\n';
+			$('#phone').focus();
+		}
+		 
+		 // 如果没有错误则提交
+		if(str != '') {
+		  alert(str);
+		  return false;
+		} else {
+//		  $('.auth-form').submit();
+			alert('完成');
+		 }
+		}
+		 
+		$('.bth').on('click', function() {
+		 formValidate();
+		});
